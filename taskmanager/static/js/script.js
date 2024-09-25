415b0deb7aa96fa225356c412c36585f645402d0
Rafal Zygula <p0hmgxcn@students.codeinstitute.net>
2024-09-25 17:43:17 +0000

    Add task adding

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });

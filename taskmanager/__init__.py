739d511c392ae1c01275e5d5c38bd7b9299bbc76
Rafal Zygula <p0hmgxcn@students.codeinstitute.net>
2024-09-25 21:04:51 +0000

    Fix type

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -12,7 +12,7 @@ if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
-    if uri.startswitch("postgres://"):
+    if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresq://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
